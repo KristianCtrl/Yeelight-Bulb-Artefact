344e60471d18a47b9d9e53f2e8b2b103f7af84e5
Kristian Murphy <55843457+KristianCtrl@users.noreply.github.com>
2022-04-04 14:04:10 +0100

    Add files via upload

---
+++ python commands/properties.py
@@ -0,0 +1,15 @@
+from yeelight import Bulb
+bulb = Bulb("IP HERE")
+
+bulb.get_properties()
+{'bright': u'10',
+ 'color_mode': u'2',
+ 'ct': u'2700',
+ 'delayoff': u'0',
+ 'flowing': u'0',
+ 'hue': u'300',
+ 'music_on': u'0',
+ 'name': u'My light',
+ 'power': u'off',
+ 'rgb': u'16737280',
+ 'sat': u'100'}
